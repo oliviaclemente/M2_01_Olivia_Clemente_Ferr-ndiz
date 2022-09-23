c8d0d3535b38bd703a927b094606b054530fc0ea
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-09-23 14:16:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+In(7): 2
+Out(7):2
+
+In(8): 3.14159
+Out(8): 3.14159
+
+In(1): 'Buenas'
+Out (1): 'Buenas'
+
+In(2): HoyEsLunes=False
+In(2): HoyEsViernes=True 
+print(HoyEsLunes) 
+print(HoyEsViernes)
+True
+False
+
+nombre1= input('Olivia')
+nombre2= input('Clemente')
+posicion_final_nombre1=len(nombre1)-1
+posicion_final_nombre2=len(nombre2)-1
+print((nombre1(ø))== nombre2(ø))
+
+nombre1= input('Olivia')
+nombre2= input('Clemente.')
+posicion_final_nombre1=len(nombre1)-1
+posicion_final_nombre2=len(nombre2)-1
+print(nombre1(ø)==nombre2(ø))
+
+In(16):cadena_texto= OliviaClemente
+print('Cadenaoriginal')
+print(cadena_texto)
+print('Tipo')
+print(type(cadena_texto))
